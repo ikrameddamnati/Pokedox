2ad19951608c87fdfe03feddbcb789947950d669
Ikram Ed-damnati <eddmnatiikram123@gmail.com>
2023-04-30 11:23:31 +0100

    Initial commit

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import { StyleSheet, View } from 'react-native';
 import PokemonList from './PokemonList';
+import LittleLemonHeader from './LittleLemonHeader';
+import LittleLemonFooter from './LittleLemonFooter';
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
+       <LittleLemonHeader />
     <PokemonList/>
+    <LittleLemonFooter/>
   </View>
   );
 }
--- PokemonList.js
+++ PokemonList.js
@@ -1,100 +1,100 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, TouchableOpacity, Modal, Image, StyleSheet, ScrollView } from 'react-native';
-import axios from 'axios';
+import { StyleSheet, Text, View, Image, TouchableOpacity, FlatList } from 'react-native';
 
-const Pokedex = () => {
-  const [pokemonList, setPokemonList] = useState([]);
-  const [selectedPokemon, setSelectedPokemon] = useState(null);
-  const [modalVisible, setModalVisible] = useState(false);
+export default function Pokedex() {
   const [pokemonTypes, setPokemonTypes] = useState([]);
+  const [pokemonList, setPokemonList] = useState([]);
+  const [selectedType, setSelectedType] = useState(null);
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
 
   useEffect(() => {
-    const fetchPokemonList = async () => {
-      const result = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151');
-      setPokemonList(result.data.results);
-    };
-    fetchPokemonList();
+    // Récupérer la liste des types de Pokémon
+    fetch('https://pokeapi.co/api/v2/type/')
+      .then((response) => response.json())
+      .then((data) => setPokemonTypes(data.results));
   }, []);
 
-  useEffect(() => {
-    const fetchPokemonTypes = async () => {
-      const types = {};
-      for (const pokemon of pokemonList) {
-        const result = await axios.get(pokemon.url);
-        const pokemonTypes = result.data.types.map((type) => type.type.name);
-        for (const type of pokemonTypes) {
-          if (!types[type]) {
-            types[type] = [result.data];
-          } else {
-            types[type].push(result.data);
-          }
-        }
-      }
-      setPokemonTypes(types);
-    };
-    fetchPokemonTypes();
-  }, [pokemonList]);
-
-  const handlePokemonSelect = async (pokemon) => {
-    const result = await axios.get(pokemon.url);
-    setSelectedPokemon(result.data);
-    setModalVisible(true);
+  const selectType = (type) => {
+    setSelectedType(type);
+    // Récupérer la liste des Pokémon correspondant au type sélectionné
+    fetch(type.url)
+      .then((response) => response.json())
+      .then((data) => setPokemonList(data.pokemon));
   };
 
-  const renderItem = ({ item }) => (
-    <TouchableOpacity style={styles.card} onPress={() => handlePokemonSelect(item)}>
-      <Text style={styles.cardText}>{item.name}</Text>
-    </TouchableOpacity>
-  );
+  const selectPokemon = (pokemon) => {
+    setSelectedPokemon(pokemon);
+  };
 
-  const renderTypeList = ({ item }) => (
-    <View style={styles.typeContainer}>
-      <Text style={styles.typeTitle}>{item.type}</Text>
-      <FlatList
-        data={item.pokemon}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.name}
-        numColumns={3}
-      />
-    </View>
-  );
+  const renderPokemonCard = ({ item }) => {
+    // Récupérer les caractéristiques du Pokémon
+    fetch(item.pokemon.url)
+      .then((response) => response.json())
+      .then((data) => {
+        selectPokemon({
+          name: data.name,
+          image: data.sprites.front_default,
+          type: data.types[0].type.name,
+          height: data.height,
+          weight: data.weight,
+        });
+      });
+
+    return (
+      <TouchableOpacity onPress={() => selectPokemon(item)}>
+        <View style={styles.header}>
+  <Text style={styles.headerText}>Pokédex</Text>
+</View>
+
+        <View style={styles.card}>
+          <Text style={styles.cardTitle}>{item.pokemon.name}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
 
   return (
     <View style={styles.container}>
-      {pokemonTypes.length > 0 ? (
-        <FlatList
-          data={Object.keys(pokemonTypes).map((type) => ({ type, pokemon: pokemonTypes[type] }))}
-          renderItem={renderTypeList}
-          keyExtractor={(item) => item.type}
-        />
-      ) : (
-        <Text>Loading...</Text>
-      )}
-      <Modal
-        animationType="slide"
-        visible={modalVisible}
-        onRequestClose={() => setModalVisible(false)}
-      >
-        {selectedPokemon && (
-          <ScrollView contentContainerStyle={styles.modalContent}>
-            <Image
-              style={styles.modalImage}
-              source={{ uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${selectedPokemon.id}.png` }}
-            />
-            <Text style={styles.modalText}>Name: {selectedPokemon.name}</Text>
-            <Text style={styles.modalText}>ID: {selectedPokemon.id}</Text>
-            <Text style={styles.modalText}>Height: {selectedPokemon.height}</Text>
-            <Text style={styles.modalText}>Weight: {selectedPokemon.weight}</Text>
-            <TouchableOpacity style={styles.modalButton} onPress={() => setModalVisible(false)}>
-              <Text style={styles.modalButtonText}>Close</Text>
-            </TouchableOpacity>
-          </ScrollView>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Pokédex</Text>
+      </View>
+      {/* Afficher la liste des types de Pokémon */}
+      <FlatList
+        data={pokemonTypes}
+        keyExtractor={(item) => item.name}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => selectType(item)}>
+            <View style={selectedType === item ? styles.selectedType : styles.type}>
+              <Text style={styles.typeText}>{item.name}</Text>
+            </View>
+          </TouchableOpacity>
         )}
-      </Modal>
+      />
+      {/* Afficher la liste des Pokémon correspondant au type sélectionné */}
+      {selectedType && (
+        <FlatList
+          data={pokemonList}
+          keyExtractor={(item) => item.pokemon.name}
+          renderItem={renderPokemonCard}
+          numColumns={2}
+        />
+      )}
+      {/* Afficher les caractéristiques du Pokémon sélectionné */}
+      {selectedPokemon && (
+        <View style={styles.popup}>
+          <Image style={styles.popupImage} source={{ uri: selectedPokemon.image }} />
+          <Text style={styles.popupText}>{selectedPokemon.name}</Text>
+          <Text style={styles.popupText}>Type: {selectedPokemon.type}</Text>
+          <Text style={styles.popupText}>Height: {selectedPokemon.height}</Text>
+          <Text style={styles.popupText}>Weight: {selectedPokemon.weight}</Text>
+          <TouchableOpacity style={styles.popupButton} onPress={() => setSelectedPokemon(null)}>
+            <Text style={styles.popupButtonText}>Close</Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   );
-};
-
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -110,36 +110,55 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#ccc',
   },
-  cardText: {
+  cardTitle: {
     fontSize: 16,
     fontWeight: 'bold',
     textAlign: 'center',
   },
-  modalContent: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
+  type: {
+    backgroundColor: '#eee',
+    padding: 10,
+    borderRadius: 5,
+    margin: 5,
   },
-  modalImage: {
+  selectedType: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+    margin: 5,
+  },
+  typeText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  popup: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: '#fff',
+    padding: 20,
+    borderTopWidth: 1,
+    borderTopColor: '#ccc',
+  },
+  popupImage: {
     width: 200,
     height: 200,
     marginBottom: 10,
   },
-  modalText: {
+  popupText: {
     fontSize: 18,
     marginVertical: 5,
   },
-  modalButton: {
+  popupButton: {
     backgroundColor: 'blue',
     padding: 10,
     borderRadius: 5,
     marginTop: 20,
   },
-  modalButtonText: {
+  popupButtonText: {
     color: '#fff',
     fontWeight: 'bold',
   },
 });
-
-export default Pokedex;
